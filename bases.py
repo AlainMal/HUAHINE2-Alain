7e48f0e475cad3bb03a775f901db6e385c134634
AlainMal <info@labase-malvoisin.net>
2025-08-05 10:52:05 +0200

    commit

---
+++ bases.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+# Connexion à la base de données "Monde"
+conn_planette = sqlite3.connect('D:/Alain/cartes1.mbtiles')
+cur_planette = conn_planette.cursor()
+
+# Attacher la base de données OpenSeaMap
+cur_planette.execute("ATTACH DATABASE 'E:/Alain/cartesC.mbtiles' AS seamap")
+
+# Copie des tuiles de OpenSeaMap (zoom 10-19) dans la base planette
+cur_planette.execute("""
+    INSERT INTO tiles (zoom_level, tile_column, tile_row, tile_data)
+    SELECT zoom_level, tile_column, tile_row, tile_data s
+    FROM seamap.tiles
+    WHERE zoom_level BETWEEN 10 AND 19
+""")
+
+# Validation des changements
+conn_planette.commit()
+
+# Détacher la base de données OpenSeaMap
+cur_planette.execute("DETACH DATABASE seamap")
+
+# Fermeture de la connexion
+cur_planette.close()
+conn_planette.close()
