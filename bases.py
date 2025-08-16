818521da1a92ffd0d2c10b5fbe76bb9e247cb28e
AlainMal <info@labase-malvoisin.net>
2025-08-16 11:05:23 +0200

    commit

--- bases.py
+++ bases.py
@@ -1,13 +1,13 @@
 import sqlite3
 
-# Connexion à la base de données "Monde"
+# Connexion à la base de données "Navionic"
 conn_planette = sqlite3.connect('D:/Alain/cartes3.mbtiles')
 cur_planette = conn_planette.cursor()
 
 # Attacher la base de données OpenSeaMap
 cur_planette.execute("ATTACH DATABASE 'E:/Alain/cartesM.mbtiles' AS seamap")
 
-# Copie des tuiles de OpenSeaMap (zoom 10-19) dans la base planette
+# Copie des tuiles de SeaMap (zoom 10-19) dans la base Navionic
 cur_planette.execute("""
     INSERT OR REPLACE INTO tiles (zoom_level, tile_column, tile_row, tile_data)
     SELECT zoom_level, tile_column, tile_row, tile_data s
